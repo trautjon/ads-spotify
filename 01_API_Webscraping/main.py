4acc4ac834fc1c9c5b98992e9649e49df6f36baf
Jonas T. <trautjon@students.zhaw.ch>
2024-05-03 20:37:20 +0200

    Webscraping effizenter gestalltet + xlsx zum gitignore hinzugefügt

--- 01_API_Webscraping/main.py
+++ 01_API_Webscraping/main.py
@@ -2,6 +2,7 @@ import api_handler as api
 import dataframe_handler as dataframe
 import webscraper as scraper
 import datetime
+import os
 
 today = datetime.datetime.now().strftime('%Y-%m-%d')
 
@@ -9,7 +10,27 @@ token = api.get_token()
 
 #playlist_user_input = input("Bitte gebe die Playlist ein, welche du suchen möchtest (Format Top 50 - LAND/Global): ")
 
-playlist_user_inputs = scraper.get_playlist_names()
+playlist_file = "Playlistnames.txt"
+
+#Um effizent zu sein wird nur gescraped, wenn keine Playlistnames.txt vorhanden ist.
+
+if os.path.exists(playlist_file):
+    # Lies die Playlist-Namen aus der Datei aus
+    with open(playlist_file, "r") as file:
+        playlist_user_inputs = file.read().split(",")
+else:
+    # Rufe scraper.get_playlist_names() auf und speichere die Playlist-Namen in der Datei
+    playlist_user_inputs = scraper.get_playlist_names()
+    with open(playlist_file, "w") as file:
+        file.write(",".join(playlist_user_inputs))
+
+
+# Testen ob es schon einen Ordner gibt
+
+pfad_zum_ordner = "./" + today
+
+if not os.path.exists(pfad_zum_ordner):
+    os.makedirs(pfad_zum_ordner)
 
 # Alle gescrapten Playlist Namen mit Daten bereichern
 for playlist in playlist_user_inputs:
@@ -29,10 +50,13 @@ for playlist in playlist_user_inputs:
 
  charts_df = dataframe.merge_basic_song_table_with_song_feautures(basic_song_table, basic_audiofeautures_table)
 
-#Provisorisch im Excelformat speichern
+ #Provisorisch im Excelformat speichern
 
- charts_df.to_excel(playlist + " " + today + ".xlsx")
+ excel_pfad = os.path.join(pfad_zum_ordner, playlist + " " + today + ".xlsx")
+ charts_df.to_excel(excel_pfad)
 
 #Zum Debuggen
 
 #print(charts_df)
+
+#Requirements kann mit "conda list --export > requirements.txt" erstellt werden
